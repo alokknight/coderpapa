2a75d376588cd6350d2e7104c339dd8c3fbb6dbf
alok patel <alokpatel885@gmail.com>
2021-11-04 22:45:03 +0530

    added all

--- coderpapa/settings.py
+++ coderpapa/settings.py
@@ -136,25 +136,25 @@ WSGI_APPLICATION = 'coderpapa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'djongo',
-#         "CLIENT": {
-#            "name": config('DB_NAME'),##database
-#            "host": config('DB_HOST'),
-#            "username": config('DB_USER'),##username
-#            "password": config('DB_PASSWORD'),##password of that database user
-#            "authMechanism": "SCRAM-SHA-1",
-#         },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        "CLIENT": {
+           "name": config('DB_NAME'),##database
+           "host": config('DB_HOST'),
+           "username": config('DB_USER'),##username
+           "password": config('DB_PASSWORD'),##password of that database user
+           "authMechanism": "SCRAM-SHA-1",
+        },
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
